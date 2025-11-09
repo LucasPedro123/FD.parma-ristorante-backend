5f7e3bdcc205cad5e699730c2ae54270b24d3b11
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-11-09 17:51:55 -0300

    feat: added DTO for user authentication

---
+++ Dtos/Users/UserDto.cs
@@ -0,0 +1,12 @@
+namespace RestaurantAPI.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
