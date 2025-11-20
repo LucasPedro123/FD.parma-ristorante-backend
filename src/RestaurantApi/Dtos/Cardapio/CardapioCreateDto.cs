05e3e6df8e33460033833efcaaa9334d729fbc28
lucasds-tech <lucas.dasilva09@hotmail.com>
2025-11-20 00:46:12 -0300

    feat: implement CardapioDtos

---
+++ src/RestaurantApi/Dtos/Cardapio/CardapioCreateDto.cs
@@ -0,0 +1,11 @@
+namespace RestaurantAPI.Dtos.Cardapio
+{
+    public class CardapioCreateDto
+    {
+        public string Nome { get; set; } = default!;
+        public string Descricao { get; set; } = default!;
+        public decimal Preco { get; set; } = default!;
+        public bool Disponivel { get; set; } = default!;
+        public string Categoria { get; set; } = default!;
+    }
+}
---
+++ src/RestaurantApi/Dtos/Cardapio/CardapioReadDto.cs
@@ -0,0 +1,12 @@
+namespace RestaurantAPI.Dtos.Cardapio
+{
+    public class CardapioReadDto
+    {
+        public int Id { get; set; } = default!;
+        public string Nome { get; set; } = default!;
+        public string Descricao { get; set; } = default!;
+        public decimal Preco { get; set; } = default!;
+        public bool Disponivel { get; set; } = default!;
+        public string Categoria { get; set; } = default!;
+    }
+}
