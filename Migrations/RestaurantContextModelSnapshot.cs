cab5fc2e035c83d7ec9f4603e56b08c81daef6a5
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-11-21 00:30:53 -0300

    feat: MenuItems added to the context.

--- Migrations/RestaurantContextModelSnapshot.cs
+++ Migrations/RestaurantContextModelSnapshot.cs
@@ -21,6 +21,34 @@ namespace RestaurantAPI.Migrations
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
+            modelBuilder.Entity("RestaurantAPI.Models.MenuItem", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                    b.Property<string>("Description")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("ImageUrl")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18,2)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("MenuItems");
+                });
+
             modelBuilder.Entity("RestaurantAPI.Models.User", b =>
                 {
                     b.Property<int>("Id")
